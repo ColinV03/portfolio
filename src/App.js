63202b6bfdfba48ba9f14b6978e2ffebdfb83026
Colin Varney <cvcolinv@gmail.com>
2021-05-03 14:30:34 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import MiniDrawer from "./Components/Header"
+import About from "./Components/About"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MiniDrawer></MiniDrawer>
+      
+      
+        <About className="aboutMe" />
+      
       </header>
+
+      
     </div>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,20 @@
+import Typography from "@material-ui/core/Typography"
+
+
+export default function About() {
+    
+    return (
+
+    <div className="aboutMe"> 
+        <Typography variant="h1" gutterBottom>Colin Varney</Typography>
+        <br></br>
+        <Typography variant="h5" gutterBottom> Web Developer</Typography>
+    </div> 
+
+
+
+    )
+
+
+    
+}
